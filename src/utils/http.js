86b9b17306abd5fa767a31d8398addb4ff6d434c
divyam234 <divyam@mnnit.ac.in>
2023-05-02 12:50:01 +0530

    update

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,17 @@
-import axios from 'redaxios'
+import axios from 'axios'
+
+function getCookie(name) {
+  return (document.cookie.match('(?:^|;)\\s*' + name.trim() + '\\s*=\\s*([^;]*?)\\s*(?:;|$)') || [])[1];
+}
 
 const http = axios.create({
   withCredentials: true
 })
+
+http.interceptors.request.use(function (config) {
+  const csrf_token = getCookie('csrf_access_token')
+  if (csrf_token) config.headers['X-CSRF-Token'] = csrf_token
+  return config;
+});
+
 export default http
