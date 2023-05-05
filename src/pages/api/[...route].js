cb0452a92000059b612c10f92d40664ee31545c7
divyam234 <divyam@mnnit.ac.in>
2023-05-05 18:53:53 +0530

    update

--- src/pages/api/[...route].js
+++ src/pages/api/[...route].js
@@ -10,8 +10,9 @@ const app = new Hono()
 app.basePath('/api')
 
 app.all('/*', async (c) => {
-    const address = `https://teldrive.bhunter.in${c.req.path}`;
-    return await fetch(address, { headers: c.req.headers, body: c.req.body })
+    const { method, headers, body, path } = c.req
+    const address = `https://teldrive.bhunter.in${path}`;
+    return await fetch(address, { method, headers, body })
 })
 
 export default handle(app)
